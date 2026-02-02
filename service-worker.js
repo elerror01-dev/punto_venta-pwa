b02259827901a416578a9c6c6a0d62c100c65086
elerror01-dev <elerror01@gmail.com>
2026-02-02 14:20:00 -0600

    Init PWA project2

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,21 @@
-const CACHE_NAME = "pwa-cache-v2";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./registro.html",
-  "./styles.css",
-  "./script.js",
-  "./manifest.json",
-  "./images/logo.png",
-  "./images/1.jpg",
-  "./images/2.jpg",
-  "./images/3.jpg",
-  "./images/4.jpg",
-  "./images/6.jpg"
+const CACHE_NAME = "snake-pwa-cache-v4";
+const FILES = [
+  "index.html",
+  "styles.css",
+  "script.js",
+  "manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
