5691a90444a146edb4141f89aeda3e149605d212
elerror01-dev <elerror01@gmail.com>
2026-02-03 11:49:20 -0600

    PWA project4

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,16 @@
 const CACHE_NAME = "acuario-pwa-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/manifest.json",
-  "/images/logo.png"
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./manifest.json",
+  "./images/logo.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
